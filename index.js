ed5ee1170665115731e0409319c2dfc70fc73c88
meetakbari <meetakbari.me@gmail.com>
2021-06-04 22:38:58 +0530

    router and index file added

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
 const express = require('express');
-const socketio = require('socket.io')
+
 const http = require('http');
 
+const {addUser, removeUser, getUser, getUsersInRoom} = require('./users');
 const PORT = process.env.PORT || 5000;
 
-const router = require('./router')
+const router = require('./router.js')
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = require('socket.io')(server, {
+    cors: {
+      origin: `http://localhost:${PORT}`,
+      methods: ["GET", "POST"],
+      credentials: true
+    }
+  });
 
 io.on('connection', (socket) => {
     console.log("We got new connection");
+    
+    socket.on('join', ({name, room}, callback) => {
+        const {error, user} = addUser({id: socket.id, name: name, room: room});
 
-    socket.on('join', ({name, room}) => {
-        console.log(name, room);
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'admin', text: `Hey ${user.name}! Welcome to room ${user.room}!` });
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} joined chat!` });
+
+        socket.join(user.room);
+
+        io.to(user.room).emit('roomData', {room: user.room, users: getUsersInRoom(user.room)});
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message});
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+
+        callback();
     })
+
     socket.on('disconnect', () => {
-        console.log("User left!");
-    })
+        const user = removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', { user: 'admin', text: `${user.name} has left!`});
+        }
+    });
 })
 
 app.use(router);
